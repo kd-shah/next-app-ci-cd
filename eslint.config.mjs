17aba5ebcf0d2c5cea814f347f78a0756510a273
Kunal Shah <kunalshah.code@gmail.com>
2025-11-09 11:25:46 +0530

    Adds next app with ci cd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,14 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    rules: {
+      // Treat unused variables as errors instead of warnings
+      "no-unused-vars": "error",
+    },
+  },
+  // Override default ignores of eslint-config-next
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
